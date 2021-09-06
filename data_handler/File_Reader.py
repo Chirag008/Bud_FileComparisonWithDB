6abc7732d721809eaae112d21b3a04fc31822d56
chirag.bansal <chirag.bansal@ihsmarkit.com>
2021-09-06 18:05:11 +0530

    added method to download file from azure storage

--- data_handler/File_Reader.py
+++ data_handler/File_Reader.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 
+from azure.storage.blob import BlobClient, BlobServiceClient
+
 
 class File_Reader:
     file_handler = None
@@ -9,13 +11,40 @@ class File_Reader:
 
     def __init__(self, file_path, separator='~'):
         self.separator = separator
-        project_root = Path(os.path.abspath(os.path.dirname(__file__))).parent
-        self.file_path = os.path.join(project_root, file_path)
         if not self.file_handler:
-            self.file_handler = open(self.file_path)
+            # Get the file from azure to local machine
+            self.get_file_from_azure_storage(file_path)
+            # Get the file handler to the downloaded file
+            self.file_handler = self.get_file_handler_from_local(file_path)
         if self.is_first_row:
             self.headers = self.file_handler.readline().rstrip('\n').split(self.separator)
             self.is_first_row = False
+
+    def get_file_from_azure_storage(self, download_file_path):
+        connection_string = "DefaultEndpointsProtocol=https;AccountName=sacmfgd02dlxdatapoc2;AccountKey=yjYSUhPn7ucdPoXa43F/bS/wHQqSbvLyZVmqieS/QVEmXRjp7MAjoufq1gqEaSGXDkCK+c4XB2HXTx7X85YYOQ==;EndpointSuffix=core.windows.net"
+        try:
+            blob = BlobClient.from_connection_string(conn_str=connection_string, container_name="aedigital",
+                                                     blob_name="cu00000001/202103/20210331")
+            print("connection to azure storage successfully!")
+        except:
+            print("problem in connecting to azure storage")
+        container_name = "aedigital"
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=download_file_path)
+        blobs_list = blob_client.list_blobs()
+
+        for blob in blobs_list:
+            print("\nDownloading blob to \n\t" + download_file_path)
+            if "cu00000001/202103/20210331/EXTRACT.ACCOUNT" in blob.name:
+                with open(download_file_path, "wb") as download_file:
+                    print(blob.name + '\n')
+                    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob)
+                    download_file.write(blob_client.download_blob().readall())
+
+    def get_file_handler_from_local(self, file_path):
+        project_root = Path(os.path.abspath(os.path.dirname(__file__))).parent
+        file_path = os.path.join(project_root, file_path)
+        return open(file_path)
 
     def get_next_row_as_dict(self):
         row = self.file_handler.readline()
